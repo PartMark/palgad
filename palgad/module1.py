a06fc4eaf10fd812d2aabed9abece869351844c1
opilane <opilane@HP-CZC2349HTY>
2024-02-28 13:38:53 +0200

    a1

--- palgad/module1.py
+++ palgad/module1.py
@@ -18,3 +18,27 @@ def naita_andmed(i:list,p:list):
     """
     for j in range(len(i)):
         print(i[j],"-",p[j])
+def andmete_kustutamine(i:list,p:list)->any:
+    """
+    """
+    nimi=input("Keda kustutada ära?(nimi) ")
+    if nimi not in i:
+        print(f"{nimi} puudub")
+    else:
+        for j in range(len(i)):
+            if nimi in i:
+                p.pop(i.index(nimi))
+                i.remove(nimi)
+    return i,p
+def kellel_on_suurim_palk(i:list,p:list)->list:
+    """
+    """
+    nimed=[]
+    max_palk=max(p)
+    ind=p.index(max_palk)
+    for palk in p:
+        if max_palk==palk:
+            nimi=i[p.index(palk,ind)]
+            nimed.append(nimi)
+            ind+=1
+    return nimed
--- palgad/palgad.py
+++ palgad/palgad.py
@@ -3,9 +3,14 @@ palgad=[1200,2500,750,395,1200]
 inimesed=["A","B","C","D","E"]
 
 while True:
-    print("0-Andmed ekraanile\n1-Admete lisamine\n")
+    print("0-Andmed ekraanile\n1-Admete lisamine\n2-Andmete eemaldamine\n3-Kellel on suurim palk\n")
     vastus=int(input())
     if vastus==0:
         naita_andmed(inimesed,palgad)
     elif vastus==1:
         inimesed,palgad=andmete_lisamine(inimesed,palgad)
+    elif vastus==2:
+        inimesed,palgad=andmete_kustutamine(inimesed,palgad)
+    elif vastus==3:
+        rikkad_inimesed=kellel_on_suurim_palk(inimesed,palgad)
+        print(rikkad_inimesed)
