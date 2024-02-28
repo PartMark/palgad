d6bb62928a91034ed188381f76366c07f717d637
opilane <opilane@HP-CZC2349HTY>
2024-02-28 14:55:52 +0200

    a1

--- palgad/module1.py
+++ palgad/module1.py
@@ -1,44 +1,82 @@
-def andmete_lisamine(i:list,p:list)->any:
-    """
-    """
-    while True:
-        try:
-            n=int(input("Mitu inimest"))
-            if n>0: break
-        except:
-            print("Viga. Proovi uuesti!")
-    for j in range(n):
-        nimi=input("Nimi: ")
-        palk=int(input("Palk: "))
-        i.append(nimi)
-        p.append(palk)
-    return i,p
-def naita_andmed(i:list,p:list):
-    """
-    """
-    for j in range(len(i)):
-        print(i[j],"-",p[j])
-def andmete_kustutamine(i:list,p:list)->any:
-    """
-    """
-    nimi=input("Keda kustutada ära?(nimi) ")
-    if nimi not in i:
-        print(f"{nimi} puudub")
+#Palgad
+#def andmete_lisamine(i:list,p:list)->any:
+#    """
+#    """
+#    while True:
+#        try:
+#            n=int(input("Mitu inimest"))
+#            if n>0: break
+#        except:
+#            print("Viga. Proovi uuesti!")
+#    for j in range(n):
+#        nimi=input("Nimi: ")
+#        palk=int(input("Palk: "))
+#        i.append(nimi)
+#        p.append(palk)
+#    return i,p
+#def naita_andmed(i:list,p:list):
+#    """
+#    """
+#    for j in range(len(i)):
+#        print(i[j],"-",p[j])
+#def andmete_kustutamine(i:list,p:list)->any:
+#    """
+#    """
+#    nimi=input("Keda kustutada ära?(nimi) ")
+#    if nimi not in i:
+#        print(f"{nimi} puudub")
+#    else:
+#        for j in range(len(i)):
+#            if nimi in i:
+#                p.pop(i.index(nimi))
+#                i.remove(nimi)
+#    return i,p
+#def kellel_on_suurim_palk(i:list,p:list)->list:
+#    """
+#    """
+#    nimed=[]
+#    max_palk=max(p)
+#    ind=p.index(max_palk)
+#    for palk in p:
+#        if max_palk==palk:
+#            nimi=i[p.index(palk,ind)]
+#            nimed.append(nimi)
+#            ind+=1
+#    return nimed
+#def sorteerimine(i:list,p:list)->any:
+#    """
+#    """
+#    for n in range(0,len(i)):
+#        for m in range(n,len(i)):
+#            if p[n]>p[m]:
+#                p[n],p[m]=p[m],p[n]
+#                i[n],i[m]=i[m],i[n]
+#    return i,p
+
+#Iseseisevtöö "Registreerimine ja autoriseerimine"
+
+def registreerimine(nimed, paroolid):
+    nimi = input("Sisesta kasutajanimi: ")
+    if nimi in nimed:
+        print("Kasutajanimi on juba võetud!")
+        return
+    valik = input("Kas soovite luua endale parooli (jah/ei)? ").lower()
+    if valik == "jah":
+        parool = input("Sisesta parool: ")
     else:
-        for j in range(len(i)):
-            if nimi in i:
-                p.pop(i.index(nimi))
-                i.remove(nimi)
-    return i,p
-def kellel_on_suurim_palk(i:list,p:list)->list:
-    """
-    """
-    nimed=[]
-    max_palk=max(p)
-    ind=p.index(max_palk)
-    for palk in p:
-        if max_palk==palk:
-            nimi=i[p.index(palk,ind)]
-            nimed.append(nimi)
-            ind+=1
-    return nimed
+        parool = salasona(12)
+        print("Teie automaatselt genereeritud parool on:", parool)
+    nimed.append(nimi)
+    paroolid.append(parool)
+    print("Kasutaja registreeritud edukalt!")
+import random
+import string
+
+def salasona(k: int):
+    sala = ""
+    for i in range(k):
+        t = random.choice(string.ascii_letters)
+        num = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+        t_num = [t, str(num)]
+        sala += random.choice(t_num)
+    return sala
--- palgad/palgad.py
+++ palgad/palgad.py
@@ -1,16 +1,56 @@
 from module1 import *
-palgad=[1200,2500,750,395,1200]
-inimesed=["A","B","C","D","E"]
+#palgad=[1200,2500,750,395,1200]
+#inimesed=["A","B","C","D","E"]
+
+#while True:
+#    print("0-Andmed ekraanile\n1-Admete lisamine\n2-Andmete eemaldamine\n3-Kellel on suurim palk\n4-Sorteerime\n")
+#    vastus=int(input())
+#    if vastus==0:
+#        naita_andmed(inimesed,palgad)
+#    elif vastus==1:
+#        inimesed,palgad=andmete_lisamine(inimesed,palgad)
+#    elif vastus==2:
+#        inimesed,palgad=andmete_kustutamine(inimesed,palgad)
+#    elif vastus==3:
+#        rikkad_inimesed=kellel_on_suurim_palk(inimesed,palgad)
+#        print(rikkad_inimesed)
+#    elif vastus==4:
+#        inimesed,palgad=sorteerimine(inimesed,palgad)
+
+#Iseseisevtöö "Registreerimine ja autoriseerimine"
+nimed = []
+paroolid = []
 
 while True:
-    print("0-Andmed ekraanile\n1-Admete lisamine\n2-Andmete eemaldamine\n3-Kellel on suurim palk\n")
-    vastus=int(input())
-    if vastus==0:
-        naita_andmed(inimesed,palgad)
-    elif vastus==1:
-        inimesed,palgad=andmete_lisamine(inimesed,palgad)
-    elif vastus==2:
-        inimesed,palgad=andmete_kustutamine(inimesed,palgad)
-    elif vastus==3:
-        rikkad_inimesed=kellel_on_suurim_palk(inimesed,palgad)
-        print(rikkad_inimesed)
+    print("\nValige tegevus:")
+    print("1. Registreerimine")
+    print("2. Autoriseerimine")
+    print("3. Nime või parooli muutmine")
+    print("4. Unustasin parooli taastamine")
+    print("5. Lõpetamine")
+
+    valik = input("Sisestage valiku number: ")
+
+    if valik == "1":
+        registreerimine(nimed, paroolid)
+    elif valik == "2":
+        nimi = input("Sisesta kasutajanimi: ")
+        parool = input("Sisesta parool: ")
+        if nimi in nimed:
+            index = nimed.index(nimi)
+            if parool == paroolid[index]:
+                print("Autoriseerimine õnnestus!")
+            else:
+                print("Vale parool!")
+        else:
+            print("Kasutajat ei leitud!")
+    elif valik == "3":
+        pass
+    elif valik == "4":
+
+        pass
+    elif valik == "5":
+        print("Programm lõpetatakse.")
+        break
+    else:
+        print("Vigane valik, palun valige uuesti.")
